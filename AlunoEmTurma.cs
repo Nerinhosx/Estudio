e88167b3058117948cbf219be7cf2d4580275499
bialemess <cl201288@g.unicamp.br>
2023-10-18 14:20:17 -0300

    Criação da classe AlunoEmTurma

---
+++ AlunoEmTurma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudio
+{
+    class AlunoEmTurma
+    {
+        private int idA;
+        private int idT;
+
+        public int IdA { get => idA; set => idA = value; }
+        public int IdT { get => idT; set => idT = value; }
+    }
+}
