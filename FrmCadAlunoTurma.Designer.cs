28539b4cdac89718d9c84498543504adad4f741c
bialemess <cl201288@g.unicamp.br>
2023-10-18 14:36:27 -0300

    Visual da tela de Cadastro: Aluno -> Turma concluída

---
+++ FrmCadAlunoTurma.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Estudio
+{
+    partial class FrmCadAlunoTurma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvTurma = new System.Windows.Forms.DataGridView();
+            this.modalidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diaSemana = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCad = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTurma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCad);
+            this.groupBox1.Controls.Add(this.dgvTurma);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCPF);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 267);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aluno/Turma";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(87, 23);
+            this.txtCPF.Mask = "000.000.000-00";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(84, 20);
+            this.txtCPF.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPF do Aluno:";
+            // 
+            // dgvTurma
+            // 
+            this.dgvTurma.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTurma.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.modalidade,
+            this.diaSemana,
+            this.hora});
+            this.dgvTurma.Location = new System.Drawing.Point(9, 63);
+            this.dgvTurma.Name = "dgvTurma";
+            this.dgvTurma.Size = new System.Drawing.Size(347, 150);
+            this.dgvTurma.TabIndex = 2;
+            // 
+            // modalidade
+            // 
+            this.modalidade.HeaderText = "Modalidade";
+            this.modalidade.Name = "modalidade";
+            // 
+            // diaSemana
+            // 
+            this.diaSemana.HeaderText = "Dia da Semana";
+            this.diaSemana.Name = "diaSemana";
+            // 
+            // hora
+            // 
+            this.hora.HeaderText = "Horário";
+            this.hora.Name = "hora";
+            // 
+            // btnCad
+            // 
+            this.btnCad.Location = new System.Drawing.Point(9, 232);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(347, 29);
+            this.btnCad.TabIndex = 3;
+            this.btnCad.Text = "Cadastrar";
+            this.btnCad.UseVisualStyleBackColor = true;
+            // 
+            // FrmCadAlunoTurma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 292);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmCadAlunoTurma";
+            this.Text = "Cadastrar Aluno em Turma";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTurma)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.Button btnCad;
+        private System.Windows.Forms.DataGridView dgvTurma;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modalidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diaSemana;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hora;
+    }
+}
---
+++ FrmCadAlunoTurma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudio
+{
+    public partial class FrmCadAlunoTurma : Form
+    {
+        public FrmCadAlunoTurma()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
